7c77597b3aaa97e16f7e1a9e3c15a0da7bb22b37
Marco Antognini <antognini.marco@gmail.com>
2015-10-17 16:56:03 +0200

    Add sections to testCode

--- src/testCode.m
+++ src/testCode.m
@@ -15,6 +15,8 @@ y = tX * beta;
 % max error
 eps = 1e-2;
 
+%%
+
 % test least squares via gradient descent
 disp('least squares via gradient descent...');
 alpha = 0.01;
@@ -22,11 +24,15 @@ tBeta = leastSquaresGD(y, tX, alpha);
 assert(all(abs(tBeta - beta) < eps));
 disp('OK!');
 
+%%
+
 % testing least squares
 disp('least squares...');
 tBeta = leastSquares(y, tX);
 assert(all(abs(tBeta - beta) < eps));
 disp('OK!');
+
+%%
 
 % testing ridge regression
 disp('ridge regression...');
@@ -34,6 +40,8 @@ lambda = 1e-5;
 tBeta = ridgeRegression(y, tX, lambda);
 assert(all(abs(tBeta - beta) < eps));
 disp('OK!');
+
+%%
 
 % generate binary data
 y = (y>0);
@@ -46,6 +54,8 @@ tY = 1.0 ./ (1.0 + exp(-tX * tBeta)) > 0.5;
 assert(sum(tY ~= y) / size(y,1) < 0.2);
 disp('OK!');
 
+%%
+
 % testing penalize logistic regression
 disp('penalized logistic regression...');
 alpha = 1e-3;
@@ -54,6 +64,8 @@ tBeta = penLogisticRegression(y,tX,alpha,lambda);
 tY = 1.0 ./ (1.0 + exp(-tX * tBeta)) > 0.5;
 assert(sum(tY ~= y) / size(y,1) < 0.2);
 disp('OK!');
+
+%%
 
 % code for writing csv files
 tY = 1.0 ./ (1.0 + exp(-tX * tBeta));
