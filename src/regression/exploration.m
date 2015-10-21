a8a6f7255c08ff9e6b92f0dad30f598f3a100d0b
Marco Antognini <antognini.marco@gmail.com>
2015-10-21 19:53:49 +0200

    Remove and move code

--- src/regression/exploration.m
+++ src/regression/exploration.m
@@ -30,7 +30,8 @@ for figNo = 0:(nbFig - 1)
     end
 end
 
-% Display histogram of response
+
+%% Display histogram of response
 figure('Name', 'histogram of response');
 hist(y_train, 100);
 xlabel('y');
@@ -38,7 +39,15 @@ ylabel('occurrences');
 title('histogram of training data');
 
 
-% Display histograms per feature
+%% Display histograms per feature
+
+nbDim = size(X_train, 2);
+plotDim = [4, 4];
+plotPerFig = (plotDim(1) * plotDim(2));
+nbFig = ceil(size(X_train, 2) / plotPerFig);
+
+assert(nbDim < nbFig * plotPerFig);
+
 for figNo = 0:(nbFig - 1)
     figure('Name', ['histograms, ' num2str(figNo + 1) ' of ' num2str(nbFig)]);
     
@@ -52,6 +61,21 @@ for figNo = 0:(nbFig - 1)
             title(['feature #' num2str(plotNo)]);
         end
     end
+end
+
+
+%% Plot histogram of Discrete features only
+groupB     = [9, 11, 15, 22, 27, 30, 38, 40, 44, 47, 56, 61];
+groupBcats = [3,  3,  2,  2,  2,  3,  3,  4,  4,  3,  4,  3];
+
+figure('Name', 'Discrete feature histograms');
+
+for i = 1:length(groupB)
+    feature = groupB(i);
+    bins = groupBcats(i);
+    subplot(3, 4, i);
+    hist(X_train(:, feature), bins);
+    title([num2str(feature) 'th feature']);
 end
 
 
@@ -110,48 +134,14 @@ plot3(X_trainN25, X_trainN62, y_train, '.');
 grid on;
 
 
-%% 3-mean: identification of the three input sources [dirty code]
-
-X_trainN25 = X_trainN(:, 25);
-X_trainN62 = X_trainN(:, 62);
-X = [X_trainN25 X_trainN62];
-C = [-0.33 -0.6; -0.26 1.5; 2.43 1.5];
-[idx, C] = kmeans(X, 3, 'MaxIter',1000, 'Start', C);
-
-X_25k1 = X_trainN25(idx == 1);
-X_25k2 = X_trainN25(idx == 2);
-X_25k3 = X_trainN25(idx == 3);
-X_62k1 = X_trainN62(idx == 1);
-X_62k2 = X_trainN62(idx == 2);
-X_62k3 = X_trainN62(idx == 3);
-y_k1 = y_train(idx == 1);
-y_k2 = y_train(idx == 2);
-y_k3 = y_train(idx == 3);
-
-figure;
-subplot(1, 2, 1);
-plot(X_25k1, y_k1, 'r.', X_25k2, y_k2, 'g.', X_25k3, y_k3, 'b.', 'MarkerSize', 15);
-grid on;
-subplot(1, 2, 2);
-plot(X_62k1, y_k1, 'r.', X_62k2, y_k2, 'g.', X_62k3, y_k3, 'b.', 'MarkerSize', 15);
-grid on;
-
-figure;
-plot3(X_25k1, X_62k1, y_k1, 'r.', 'MarkerSize', 15);
-hold on;
-plot3(X_25k2, X_62k2, y_k2, 'g.', 'MarkerSize', 15);
-plot3(X_25k3, X_62k3, y_k3, 'b.', 'MarkerSize', 15);
-hold off;
-grid on;
-
-
 %% K-Means of features 25 & 62
 
 K = 3;
 X25 = X_train(:, 25);
 X62 = X_train(:, 62);
 X = [X25 X62];
-[idx, C] = kmeans(X, K, 'MaxIter', 1000, 'Distance', 'cityblock');
+C = [-0.33 -0.6; -0.26 1.5; 2.43 1.5];
+[idx, C] = kmeans(X, K, 'MaxIter', 1000, 'Distance', 'cityblock', 'Start', C);
 
 % 3D Plot of feature 25 & 62 with clustering
 figure('Name', [num2str(K) '-Means of feature 25 & 62']);
@@ -165,7 +155,7 @@ zlabel('response');
 grid on;
 
 
-%% clusterdata
+%% clusterdata of features 25 & 62
 
 K = 3;
 X25 = X_train(:, 25);
@@ -185,7 +175,7 @@ zlabel('response');
 grid on;
 
 
-%% fitgmdist
+%% fitgmdist of features 25 & 62
 
 K = 3;
 X25 = X_train(:, 25);
@@ -206,7 +196,7 @@ zlabel('response');
 grid on;
 
 
-%% Clustering by hand
+%% Clustering by hand of features 25 & 62
 
 X25 = X_train(:, 25);
 X62 = X_train(:, 62);
@@ -230,7 +220,7 @@ grid on;
 axis square;
 
 
-%% Clustering using response for validation of our manual splitting
+%% Clustering using response for validation of manual splitting of features 25 & 62
 
 K = 3;
 X25 = X_train(:, 25);
@@ -290,60 +280,3 @@ for figNo = 0:(nbFig - 1)
         end
     end
 end
-
-
-%% Plot histogram of Discrete features only
-groupB     = [9, 11, 15, 22, 27, 30, 38, 40, 44, 47, 56, 61];
-groupBcats = [3,  3,  2,  2,  2,  3,  3,  4,  4,  3,  4,  3];
-
-figure('Name', 'Discrete feature histograms');
-
-for i = 1:length(groupB)
-    feature = groupB(i);
-    bins = groupBcats(i);
-    subplot(3, 4, i);
-    hist(X_train(:, feature), bins);
-    title([num2str(feature) 'th feature']);
-end
-
-
-%% Clustering using a categorical feature [No good]
-
-cat = 61; % one of groupB with three categories, but none produced good clustering
-X_cat = X_train(:, cat);
-K = 3;
-
-idx = kmeans(X_cat, K);
-
-figure;
-for k = 1:K
-    plot(X_cat(idx == k), y_train(idx == k), '.', 'MarkerSize', 15);
-    hold on;
-end
-
-% Plot all features with clustered data
-nbDim = size(X_train, 2);
-plotDim = [4, 4];
-plotPerFig = (plotDim(1) * plotDim(2));
-nbFig = ceil(size(X_train, 2) / plotPerFig);
-
-assert(nbDim < nbFig * plotPerFig);
-
-for figNo = 0:(nbFig - 1)
-    figure('Name', ['clustered raw data, ' num2str(figNo + 1) ' of ' num2str(nbFig)]);
-
-    for subplotNo = 1:plotPerFig
-        plotNo = figNo * plotPerFig + subplotNo;
-        
-        if plotNo <= nbDim
-            subplot(plotDim(1), plotDim(2), subplotNo);
-
-            for k = 1:K
-                plot(X_train(idx == k, plotNo), y_train(idx == k), '.');
-                hold on;
-            end
-            title(['feature #' num2str(plotNo)]);
-        end
-    end
-end
-
