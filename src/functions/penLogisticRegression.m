de978ad83b1fe49ef94ca375968d63624717dae5
pacm <pacm@users.github.com>
2015-10-22 16:05:58 +0200

    implement penalized Logistic Regression #5

---
+++ src/functions/penLogisticRegression.m
@@ -0,0 +1,16 @@
+function beta = penLogisticRegression(y,tX,alpha,lambda)
+% penLogisticRegression(y,tX,alpha,lambda)
+%   Compute the penalized logistic regression using Newton's method.
+%
+    % initialization
+    [~, M] = size(tX);
+    beta0 = zeros(M, 1); % aka beta0
+    precision = 1^(-10);
+    maxIters = 100;
+    
+    % Newton's method is equivalent to gradient descent with a custom
+    % gradient taking into account second-order information
+    customGradient = @(y, tX, beta) logisticRegHessian(tX, beta) \ logisticRegGradient(y, tX, beta);
+    beta = gradientDescent(y, tX, beta0, alpha, maxIters, precision, customGradient);
+end
+
