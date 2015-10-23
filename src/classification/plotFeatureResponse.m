b3ae47a941e240a0b91178121cbef0bebed6a15a
Marco Antognini <antognini.marco@gmail.com>
2015-10-23 20:18:31 +0200

    Add plotFeatureResponse function for classification exploration (#13)

---
+++ src/classification/plotFeatureResponse.m
@@ -0,0 +1,23 @@
+function plotFeatureResponse(feature, X_train, y_train)
+%PLOTFEATURERESPONSE Plot the distribution on the given features with
+%respect to the response
+%
+%   This function assumes a figure/subplot is open/selected.
+%
+
+    data_train = X_train(:, feature);
+    
+    h = histogram2(data_train, y_train);
+    h.Normalization = 'probability';
+    
+    legend('train', 'Location', 'northeast');
+    
+    xlabel('input');
+    ylabel('response');
+    zlabel('probability');
+    %zlim([0 1]);
+    
+    title([num2str(feature) 'th feature']);
+
+end
+
