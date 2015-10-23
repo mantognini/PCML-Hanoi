955c7c54304434f5494404ccf1cf24d04a5b64cf
pacm <pacm@users.noreply.github.com>
2015-10-23 10:35:47 +0200

    code for generic method test (#6)

---
+++ src/regression/testMethods.m
@@ -0,0 +1,40 @@
+% Test various methods for regression dataset
+clear all;
+
+% load dataset
+allData = loadRegressionData();
+
+% initialization
+methods = {@naiveConstant};
+seeds = 10;
+prop = 0.7;
+
+N = size(allData.original.train.X, 1);
+
+% Compute expected error estimate (independent of training dataset)
+for seed = 1:seeds
+    setSeed(seed);
+    
+    % Split data into training and validation sets
+    idx = randperm(N);
+    X = allData.original.train.X(idx, :);
+    y = allData.original.train.y(idx);
+    
+    [XTr, yTr, XValid, yValid] = doSplit(y, X, prop);
+    
+    % Test each method
+    for methodNo = 1:numel(methods)
+        method = methods{methodNo};
+        
+        % Collect predictions
+        yValidPred = method(XTr, yTr, XValid);
+        
+        % Compute error
+        rmse(seed, methodNo) = computeRmse(yValidPred - yValid);
+    end
+end
+
+% Plot methods
+fig = figure();
+
+boxplot(rmse);
