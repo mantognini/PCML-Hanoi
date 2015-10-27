f3dd62c46bb757f8699df560bb1299a30693c6c8
Marco Antognini <antognini.marco@gmail.com>
2015-10-27 18:54:44 +0100

    Clear code

--- src/regression2/explo.m
+++ src/regression2/explo.m
@@ -6,7 +6,9 @@ polo = Polo();
 
 % Found by removing one feature after the other manually on 1+x+x^2+x^3
 % model (x per feature) (37 features)
-features{1} = [ 4  6  7  10  11  12  13  14  16  19  21  22  24  26  28  29  30  35  36  37  38  42  44  45  46  49  50  51  53  54  55  56  57  58  59  64  66 ];
+features{1} = [ 4  6  7  10  11  12  13  14  16  19  21  22  24  26  28 ...
+    29  30  35  36  37  38  42  44  45  46  49  50  51  53  54  55  56 ...
+    57  58  59  64  66 ];
 
 % Best using 1+x+x^2+x^3 model
 features{2} = setdiff(1:67, [... % all features minus the following:
@@ -22,32 +24,27 @@ features{2} = setdiff(1:67, [... % all features minus the following:
 features{3} = [ 4 5 6 10 20 21 26 27 30 31 32 34 35 36 37 45 46 49 50 51 53 54 56 59 63 65 ];
 
 methods = {
-    %@polo.constantMethod,
-    %@medianMethod,
-    %@meanMethod,
-    %@GDLSMethod,
-    %@ridgeLinear10Fold,
-    @polo.basisFunctionsMethod,
+    %{ trim features, algo, name }
+    %{ false, @polo.constantMethod, 'constant' },
+    { false, @medianMethod, 'median' },
+    { false, @meanMethod, 'mean' },
+    { false, @GDLSMethod, 'GD+LS' },
+    { true, @GDLSMethod, 'GD+LS + trim features' },
+    { true, @ridgeLinear10Fold, 'ridge linear + trim features' },
+    { false, @ridgeLinear10Fold, 'ridge linear' },
+    { true, @polo.basisFunctionsMethod, 'PHI + trim features' },
+    { false, @polo.basisFunctionsMethod, 'PHI' },
 };
+methodNames = cellfun(@(mInfo) mInfo{3}, methods, 'UniformOutput', false);
 
 M = numel(methods);
 S = 10;
 splitRatio = 0.7;
 
-k = 3;
-features{k} = [1 1:67];
-best = 100000;
-for suspect = features{k}
-    clear e rmse;
-    oldFs = features{k};
-    features{k} = setdiff(features{k}, suspect);
-    
-    fprintf(['processing suspect ' num2str(suspect) '\n']);
+fprintf('I am starting...\n');
 
-%fprintf('I am starting...\n');
-
-%for k = 1%1:K
-    %fprintf(['processing cluster ' num2str(k) ' of ' num2str(K) '\n']);
+for k = 1:K
+    fprintf(['processing cluster ' num2str(k) ' of ' num2str(K) '\n']);
     
     cluster = clusters{k};
     cluster = polo.deleteYOutliers(cluster);
@@ -55,28 +52,31 @@ for suspect = features{k}
 %     polo.plotData(['Cluster ' num2str(k)], cluster);
 %     continue;
     
-    cluster = polo.trimFeatures(cluster, features{k});
-    
     % Test each methods several times with different training and 
     % validation split of the data
-    fprintf('\tprocessing seed ');
-    clear e rmse;
-    for seed = 1:S
-        fprintf([num2str(seed) '  ']);
-        setSeed(seed);
+    for methodNo = 1:numel(methods)
+        fprintf(['\tprocessing method ' num2str(methodNo) ' of ' num2str(numel(methods)) '\n']);
+        mInfo = methods{methodNo};
+        trim = mInfo{1};
+        method = mInfo{2};
 
-        % Split data into training and validation sets
-        N = size(cluster.train.X, 1);
-        idx = randperm(N);
-        X = cluster.train.X(idx, :);
-        y = cluster.train.y(idx);
+        if (trim)
+            fprintf(['\ttrimming features down to ' num2str(length(features{k})) '\n']);
+            cluster = polo.trimFeatures(cluster, features{k});
+        end
+    
+        fprintf(['\t\tprocessing seed [' num2str(S) '] ']);
+        for seed = 1:S
+            fprintf([num2str(seed) '  ']);
+            setSeed(seed);
 
-        [XTr, yTr, XValid, yValid] = doSplit(y, X, splitRatio);
-        
-        % Test each method
-        for methodNo = 1:numel(methods)
-            %fprintf(['\t\tprocessing method ' num2str(methodNo) ' of ' num2str(numel(methods)) '\n']);
-            method = methods{methodNo};
+            % Split data into training and validation sets
+            N = size(cluster.train.X, 1);
+            idx = randperm(N);
+            X = cluster.train.X(idx, :);
+            y = cluster.train.y(idx);
+
+            [XTr, yTr, XValid, yValid] = doSplit(y, X, splitRatio);
 
             % Collect predictions
             yValidPred = method(XTr, yTr, XValid);
@@ -84,6 +84,8 @@ for suspect = features{k}
             % Compute error
             rmse(k, seed, methodNo) = computeRmse(yValidPred - yValid);
         end
+        
+        fprintf('\n');
     end
         
     % Plot RMSE for this cluster
@@ -91,14 +93,13 @@ for suspect = features{k}
     tmp = rmse(k, :, :);
     tmp = reshape(tmp, S, M);
     boxplot(tmp);
-    names = cellfun(@func2str, methods, 'UniformOutput', false);
-    legend(findobj(gca,'Tag','Box'), names);
+    legend(findobj(gca,'Tag','Box'), methodNames);
     title([num2str(k) 'th cluster']);
     xlabel('methods');
     ylabel('RMSE');
-%end
+end
 
-fprintf('\nI am nearly finished...\n');
+fprintf('I am nearly finished...\n');
 
 % Combine each cluster error together
 e = rmse .^ 2;
@@ -106,32 +107,12 @@ e = mean(e, 1);
 e = e .^ 0.5;
 e = reshape(e, S, M);
 
-% figure('Name', 'Overall RMSE');
-% boxplot(e, 1:M);
-% names = cellfun(@func2str, methods, 'UniformOutput', false);
-% legend(findobj(gca,'Tag','Box'), names);
-% xlabel('methods');
-% ylabel('RMSE');
-% %ylim([30 40]);
-% 
-% fprintf('I am finished now!\n');
+figure('Name', 'Overall RMSE');
+boxplot(e, 1:M);
+legend(findobj(gca,'Tag','Box'), methodNames);
+xlabel('methods');
+ylabel('RMSE');
 
-med = median(e);
-fprintf(['suspect: ' num2str(suspect) ' - median: ' num2str(med) ' for (' ...
-    num2str(length(features{k})) ') ' num2str(features{k}) '.\n']);
+fprintf('I am finished now!\n');
 
-if (med < best)
-    fprintf('\tbetter!\n');
-    best = med;
-elseif (med > best)
-    fprintf('\tworse\n');
-    features{k} = oldFs;
-else
-    fprintf('\tequal\n');
-    features{k} = oldFs;
-end
-
-end % masta loop on suspect
-
-display(features{k});
 
