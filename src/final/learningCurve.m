db48e06c966e0abf902756d12c16a6bd53117321
Marco Antognini <antognini.marco@gmail.com>
2015-10-31 14:33:08 +0100

    Update learning curve

--- src/final/learningCurve.m
+++ src/final/learningCurve.m
@@ -3,15 +3,15 @@ clear all;
 load('HaNoi_regression.mat');
 
 % initial parameters
-ratios = .4:.1:0.9;
+ratios = .6:.025:0.9;
 seeds = 1:10;
 
 % define method
-obj = FinalMethod();
-method = @obj.apply;
+method = @finalMethod;
 
 % Compute
 validRMSE = zeros(length(seeds), length(ratios));
+trainRMSE = zeros(length(seeds), length(ratios));
 for ratio = ratios
     ratioId = find(ratios == ratio);
     for seed = seeds
@@ -19,27 +19,28 @@ for ratio = ratios
         setSeed(seed);
 
         % Split data into training and validation sets
-        N = size(X_train, 1);
-        idx = randperm(N);
-        X = X_train(idx, :);
-        y = y_train(idx);
-        [XTr, yTr, XValid, yValid] = doSplit(y, X, ratio);
+        [XTr, yTr, XValid, yValid] = doSplit(y_train, X_train, ratio);
 
         % Collect predictions
-        [yValidPred, ~, testRMSE] = method(XTr, yTr, XValid, X_test);
+        [yValidPred, ~, rmseTr] = method(XTr, yTr, XValid, X_test);
 
         % Compute error for this cluster
         validRMSE(seed, ratioId) = computeRmse(yValidPred - yValid);
-        testRMSE(seed, ratioId) = testRMSE;
+        trainRMSE(seed, ratioId) = rmseTr;
     end
 end
 
 % todo: plot
-figure('Learning curve');
-boxplot(validRMSE, ratios, 'colors', 'r');
+%%
+figure('Name', 'Learning curve');
+boxplot(validRMSE, 'color', 'b', 'plotstyle', 'compact', 'labels', ratios);
+% legend(findobj(gca,'Tag','Box'), 'valid');
+%legend('valid');
 hold on;
-boxplot(validRMSE, ratios, 'colors', 'b');
-hold off;
+boxplot(trainRMSE, 'color', 'r', 'plotstyle', 'compact', 'labels', ratios);
+% legend(findobj(gca,'Tag','Box'), 'train');
+% legend(findobj(gca,'Tag','Box'), 'train', 'valid');
 xlabel('Training set size');
 ylabel('RMSE');
+ylim([200 800]);
 
