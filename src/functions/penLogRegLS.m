6ceaddd88584c6cccc4b40a48c1dd3cd05fbb2cd
pacm <pacm@users.noreply.github.com>
2015-11-01 18:33:57 +0100

    penLogReg with line search

---
+++ src/functions/penLogRegLS.m
@@ -0,0 +1,13 @@
+function beta = penLogisticRegression(y,tX,lambda)
+    % initialization
+    [~, M] = size(tX);
+    beta0 = zeros(M, 1); % aka beta0
+    precision = 1^(-10);
+    maxIters = 100;
+    
+    % Newton's method is equivalent to gradient descent with a custom
+    % gradient taking into account second-order information
+    customGradient = @(y, tX, beta) logisticRegHessian(tX, beta) \ logisticRegGradient(y, tX, beta);
+    beta = GDLS(y, tX, beta0, maxIters, precision, customGradient);
+end
+
