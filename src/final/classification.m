b7c478873612f2e6af168069bee317189a04ab32
Marco Antognini <antognini.marco@gmail.com>
2015-11-02 17:22:49 +0100

    Minor update

--- src/final/classification.m
+++ src/final/classification.m
@@ -1,35 +1,5 @@
 % Given the best learners for classification, compute rmse
-clear all;
-load('HaNoi_classification.mat');
-
-% initial parameters
-FinalSeed = 93726;
-splitRatio = 0.7;
-
-% define method
-method = @finalMethodClassifiction;
-
-% A final seed
-setSeed(FinalSeed);
-
-% Split data into training and validation sets
-N = size(X_train, 1);
-idx = randperm(N);
-X = X_train(idx, :);
-y = y_train(idx);
-[XTr, yTr, XValid, yValid] = doSplit(y, X, splitRatio);
-
-% Collect predictions
-[yValidPred, yTestPred, testRMSE] = method(XTr, yTr, XValid, X_test);
-
-% Compute 0-1 loss
-% todo
-
-% save yTestPred
-csvwrite('predictions_classification.csv', yTestPred);
-
-
-%% Quick'n'Dirty adaptation of testStrategies for final method
+% Quick'n'Dirty adaptation of testStrategies for final method
 
 clear all;
 data = loadClassificationData();
