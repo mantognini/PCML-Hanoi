92ad7160e9d0d66dca7a30a34b6e9092089d36b0
pacm <pacm@users.noreply.github.com>
2015-12-03 14:57:35 +0100

    BER Multiclass test

--- project-2/our-code/tests/testBER.m
+++ project-2/our-code/tests/testBER.m
@@ -39,4 +39,23 @@ eps = 0.0001;
 ber = BER([negSamples; posSamples], [predNeg1; predNeg2; predPos1; predPos2]);
 assert((ber - 0.4) < eps); % ber is average error percentage on each class
 
+%% Multiclass case
+
+p1 = ones(20, 1) * 1;
+p2 = ones(30, 1) * 2;
+p3 = ones(50, 1) * 3;
+
+guess1Pos = ones(4, 1) * 1; % 20% are correct guess
+guess1Neg = zeros(16, 1);
+
+guess2Pos = ones(18, 1) * 2; % 60% are correct guess
+guess2Neg = zeros(12, 1);
+
+guess3Pos = ones(20, 1) * 3; % 40% are correct guess
+guess3Neg = zeros(30, 1);
+
+% Then (80+40+60)/3 = 180/3 = 60 % are wrong guess overall
+eps = 0.0001;
+ber = BER([p1; p2; p3], [guess1Pos; guess1Neg; guess2Pos; guess2Neg; guess3Pos; guess3Neg]);
+assert((ber - 0.6) < eps); % ber is average error percentage on each class
 
