d34b5e5cc72082fda110f12c51bae2cf4c2b47db
pacm <pacm@users.noreply.github.com>
2015-12-03 13:45:13 +0100

    BER advanced test cases

--- project-2/our-code/tests/testBER.m
+++ project-2/our-code/tests/testBER.m
@@ -13,3 +13,20 @@ cunningPredictions = ones(100, 1);
 
 assert(BER([negSamples; posSamples], naivePredictions) == 0.5);
 assert(BER([negSamples; posSamples], cunningPredictions) == 0.5);
+
+%% Advanced cases
+
+posSamples = ones(10, 1); % 10% positive samples
+negSamples = zeros(90, 1); % 90% negative samples
+
+n1 = 54; % 0.6 * 90
+predNeg1 = zeros(n1, 1); % 60% of negative samples are correctly guessed
+predNeg2 = ones(90 - 54, 1); % 40% are wrongly guessed
+
+n2 = 6; % 0.6 * 11
+predPos1 = ones(6, 1); % 60% of positive samples are correctly guessed
+predPos2 = zeros(4, 1); % 40% are wrongly guessed
+
+ber = BER([negSamples; posSamples], [predNeg1; predNeg2; predPos1; predPos2]);
+assert(ber == 0.4); % ber is average error percentage on each class
+
