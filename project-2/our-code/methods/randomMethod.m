b8b18cae77d4e8bd476efe9f1e6cf98c4f6eed1e
pacm <pacm@users.noreply.github.com>
2015-12-05 17:31:58 +0100

    random method

---
+++ project-2/our-code/methods/randomMethod.m
@@ -0,0 +1,7 @@
+function yPred = randomMethod(train, XValid)
+    NValid = size(XValid.hog, 1);
+    
+    % random predictions
+    yValues = unique(train.y);
+    yPred = yValues(floor(rand(NValid, 1) * length(yValues) + 1));
+end
