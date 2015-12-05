93c3fd334dd663cd737c79be0193bec32d95043c
pacm <pacm@users.noreply.github.com>
2015-12-05 17:51:42 +0100

    plot BER on several runs

--- project-2/our-code/benchmark.m
+++ project-2/our-code/benchmark.m
@@ -8,30 +8,39 @@ clearvars;
 addpath(genpath('data/train/'));
 load 'data/train/train.mat';
 
-%% 
+%% Evaluating methods
 methods = {@randomMethod};
+nbRuns = 10;
+error = zeros(nbRuns, length(methods));
 
-% Split the data
-N = size(train.y, 1);
-ratio = 0.7;
-splitIdx = floor(N * ratio);
+for r = 1:nbRuns
+    % Split the data
+    N = size(train.y, 1);
+    ratio = 0.7;
+    splitIdx = floor(N * ratio);
 
-idx = randperm(N);
-idxTrain = idx(1:splitIdx);
-idxValid = idx(splitIdx + 1:end);
+    idx = randperm(N);
+    idxTrain = idx(1:splitIdx);
+    idxValid = idx(splitIdx + 1:end);
 
-data.train.X.hog = train.X_hog(idxTrain, :);
-data.train.X.cnn = train.X_cnn(idxTrain, :);
-data.train.y = train.y(idxTrain);
+    data.train.X.hog = train.X_hog(idxTrain, :);
+    data.train.X.cnn = train.X_cnn(idxTrain, :);
+    data.train.y = train.y(idxTrain);
 
-data.valid.X.hog = train.X_hog(idxValid, :);
-data.valid.X.cnn = train.X_cnn(idxValid, :);
-data.valid.y = train.y(idxValid);
+    data.valid.X.hog = train.X_hog(idxValid, :);
+    data.valid.X.cnn = train.X_cnn(idxValid, :);
+    data.valid.y = train.y(idxValid);
 
-for m = 1:length(methods)
-    method = methods{m};
-    
-    yPred = method(data.train, data.valid.X);
-    error = BER(yPred, data.valid.y);
+    for m = 1:length(methods)
+        method = methods{m};
+
+        yPred = method(data.train, data.valid.X);
+        error(r, m) = BER(yPred, data.valid.y);
+    end
 end
 
+
+%% Plotting scores
+figure('Name', 'BER errors');
+labels = cellfun(@func2str, methods, 'UniformOutput', false);
+boxplot(sum(error, 2), 'labels', labels);
