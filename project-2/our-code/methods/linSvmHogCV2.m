58f8653610613156001205f3f35fe1c4e47a9ce0
pacm <pacm@users.noreply.github.com>
2015-12-07 21:11:38 +0100

    linear svm on hog with cross validation on C

---
+++ project-2/our-code/methods/linSvmHogCV2.m
@@ -0,0 +1,22 @@
+function yPred = linSvmHogCV2(train, XValid)
+    % Make y binary
+    train.y = toBinary(train.y);
+    
+    % Find best C
+    C = linspace(10^(-5), 10^(-3), 10)';
+    [CStar, errStar, errors] = crossValid(train.X.hog, train.y, 10, C, @linSvmF2, @BER);
+    
+    % Predict
+    yPred = linSvmF2(train.X.hog, train.y, XValid.hog, CStar);
+    
+    % Optionally
+    % Plot the cross-validation
+    figure('Name', 'Cross-valid, linear SVM, HOG, Binary');
+    plot(C, errors, 'r-o');
+    hold on;
+    legend('ber error', 'Location', 'best');
+    plot(CStar, errStar, 'black-diamond', 'MarkerSize', 10, 'MarkerFaceColor', 'k');
+    xlabel('C');
+    ylabel('BER');
+    hold off;
+end
