0ab79878cb56ecb0cb601317134d9e3e17fc3a3c
Marco Antognini <antognini.marco@gmail.com>
2015-12-08 15:18:40 +0100

    Add all in one for CNN + PCA + NN

--- project-2/our-code/explorations/PCAexploration_CNN.m
+++ project-2/our-code/explorations/PCAexploration_CNN.m
@@ -247,3 +247,139 @@ title('diff');
 %       different input: each row is a sample of the test set, each
 %       column corresponds to a classification label and the color
 %       represents the probability of being in this or that category.
+
+
+%% ALL IN ONE: from loading to prediction
+% Here we do only the minimal work, that is we don't compute extra
+% eigenvalues or eigenvectors. This is essentially what we'll be doing in
+% the "method" function that use PCA + NN.
+
+clearvars;
+
+% Load features and labels of training data
+addpath(genpath('data/train/'));
+addpath(genpath('toolboxs/DeepLearnToolbox-master/'));
+load 'data/train/train.mat';
+train.X_cnn = sparse(double(train.X_cnn));
+
+% SETTINGS:
+M = 200;
+ratio = 0.7;
+
+% IMPLEMENTATION
+tstart = tic;
+fprintf('Splitting into train/test & normalising...\n');
+tic
+
+Tr = [];
+Te = [];
+
+idx = randperm(size(train.X_cnn,1));
+mid = floor(length(idx) * ratio);
+
+Tr.idxs = idx(1:mid);
+Tr.X = train.X_cnn(Tr.idxs,:);
+Tr.y = train.y(Tr.idxs);
+
+Te.idxs = idx(mid+1:end);
+Te.X = train.X_cnn(Te.idxs,:);
+Te.y = train.y(Te.idxs);
+
+[Tr.normX, mu, sigma] = zscore(Tr.X);
+Te.normX = normalize(Te.X, mu, sigma);
+
+clear idx mid mu sigma;
+toc
+
+
+% USING MATLAB FUNCTIONS IS TOOOOOO SLOW!
+% fprintf('Extracting data & computing eigenvectors...\n');
+% tic
+% % ON NORMALISED DATA!
+% S = cov(Tr.normX, 1); % covariance matrix normalised by the number of samples N
+% % the `1` above define the normalisation factor to 1/N
+% 
+% [Um, ~] = eigs(S, M); % the M largest eigenvectors
+% toc
+
+fprintf('Extracting data & computing eigenvectors...\n');
+tic
+% X is so big that computing the whole covariance matrix is not possible.
+% See Bishop 12.1.4 for details.
+% The main idea being that having N > D will result in D - N + 1
+% eigenvalues with value 0 and to find the non-zero eignevalues from a
+% different matrix.
+Xavg = mean(Tr.normX, 1); % per features
+N = size(Tr.normX, 1);
+D = size(Tr.normX, 2);
+X2 = Tr.normX - ones(N, 1) * Xavg; % no longer sparse...
+S = X2 * X2' / N;
+[Vm, Dm] = eigs(S, M); % the M largest eigenvectors
+lm = Dm(1:size(Dm,1)+1:end); % or Dm * ones(M, 1)
+toc
+
+% Compute U's, i.e. eigenvector for X and not X2
+fprintf('Computing eigenvalues for original huge matrix...\n');
+tic
+norm = sqrt(N * lm)';
+% Um = tmp ./ norm;
+tmp = X2' * Vm;
+Um = zeros(size(tmp));
+for i = 1:size(tmp, 2)
+    Um(:, i) = tmp(:, i) ./ norm(i);
+end
+clear tmp i;
+toc
+
+fprintf('Training simple neural network...\n');
+
+% convert X to subspace of size M
+Tr.normZ = Tr.normX * Um;
+Te.normZ = Te.normX * Um;
+
+% Setup NN.
+inputSize  = M;
+innerSize  = 100;
+outputSize = 4;
+nn = nnsetup([inputSize innerSize outputSize]);
+
+opts.numepochs  = 30;
+opts.batchsize  = 100;
+opts.plot       = 1;
+nn.learningRate = 2;
+
+% this neural network implementation requires number of samples to be a
+% multiple of batchsize, so we remove some for this to be true.
+numSampToUse = opts.batchsize * floor(size(Tr.normZ) / opts.batchsize);
+Tr.normZ     = Tr.normZ(1:numSampToUse, :);
+labels       = Tr.y(1:numSampToUse);
+
+% prepare labels for NN
+LL = [1 * (labels == 1), ... % first column, p(y=1)
+      1 * (labels == 2), ... % second column, p(y=2), etc
+      1 * (labels == 3), ...
+      1 * (labels == 4) ];
+
+fprintf('Training NN...\n');
+ttrain = tic;
+[nn, ~] = nntrain(nn, Tr.normZ, LL, opts);
+toc(ttrain)
+
+% to get the scores we need to do nnff (feed-forward)
+nn.testing = 1;
+nn = nnff(nn, Te.normZ, zeros(size(Te.normZ, 1), nn.size(end)));
+nn.testing = 0;
+
+% predict on the test set
+nnPredZ = nn.a{end};
+[~, predictionsZ] = max(nnPredZ, [], 2); % get the most probable class
+
+fprintf('total time: ');
+toc(tstart)
+
+% display results
+berErrZ = BER(Te.y, predictionsZ);
+fprintf('\nBER Testing error  Z: %.2f%%\n', berErrZ * 100);
+figure('Name', ['NN on HOG + PCA, M = ' num2str(M)]);
+imagesc(nnPredZ); colorbar;
+title(['BER(Z) = ' num2str(berErrZ)]);
