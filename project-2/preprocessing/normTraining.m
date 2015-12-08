b4348f1c113c5e60f886b8ad6f21090cad268b84
pacm <pacm@users.noreply.github.com>
2015-12-08 14:42:33 +0100

    script for data normalization

---
+++ project-2/preprocessing/normTraining.m
@@ -0,0 +1,22 @@
+%
+% Normalize training data
+
+clearvars;
+addpath(genpath('data/train/'));
+load 'data/train/train.mat';
+
+% normalize
+[data.cnn.train.X, muCnn, sigmaCnn] = zscore(train.X_cnn);
+[data.hog.train.X, muHog, sigmaHog] = zscore(train.X_hog);
+data.yTrain = train.y;
+
+% save distribution parameters
+data.cnn.mu = muCnn;
+data.hog.mu = muHog;
+
+data.cnn.sigma = sigmaCnn;
+data.hog.sigma = sigmaHog;
+
+% save to file
+save 'data.mat' data;
+
