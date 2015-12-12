8618a026a3e0ce8e6277044b0de95a1c69a9afe7
pacm <pacm@users.noreply.github.com>
2015-12-12 18:59:40 +0100

    fix svm-pca

--- project-2/our-code/functions/svmPca.m
+++ project-2/our-code/functions/svmPca.m
@@ -6,6 +6,6 @@ function yPred = svmPca(X, y, XValid, M, kernelF, C, params)
     [X2, XValid2] = pcaManual(X, XValid, M);
     
     % svm
-    yPred = svmF(X2, y, XValid2, @linearKernel, C, params);
+    yPred = svmF(X2, y, XValid2, kernelF, C, params);
     
 end
