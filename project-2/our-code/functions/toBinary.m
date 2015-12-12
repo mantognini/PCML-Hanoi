92efbf7a67676ff1e2ccd0b52b45d7a2b5f2bef6
Marco Antognini <antognini.marco@gmail.com>
2015-12-12 19:00:52 +0100

    Generalise toBinary

--- project-2/our-code/functions/toBinary.m
+++ project-2/our-code/functions/toBinary.m
@@ -1,8 +1,16 @@
-function y = toBinary(y)
+function y = toBinary(y, optClass)
 %
 % Convert 4-class y {1, 2, 3, 4} to binary y {0, 1}
-% Map 4 to 1 and {1, 2, 3} to -> 0
-    otherIdx = (y == 4);
-    y(otherIdx) = 0;
+
+    if (nargin == 1)
+        % By default map 4 to 1 and {1, 2, 3} to -> 0
+        class = 4;
+    else
+        class = optClass;
+    end
+
+    otherIdx = (y == class);
+    y( otherIdx) = 0;
     y(~otherIdx) = 1;
+
 end
