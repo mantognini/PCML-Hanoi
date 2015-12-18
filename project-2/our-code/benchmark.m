f57ae962d63c8dd0cc98fac5e6ac66bb1cd1e43f
pacm <pacm@users.noreply.github.com>
2015-12-18 13:09:17 +0100

    outsource results to this link:
    https://docs.google.com/spreadsheets/d/1fkAm75-tErmjHf7i1WEL5LCOMIJCCfjHCLJM4chZsK4/edit?usp=sharing

--- project-2/our-code/benchmark.m
+++ project-2/our-code/benchmark.m
@@ -15,38 +15,8 @@ nbRuns = 5;
 ratio = 0.7;
 
 %% Evaluating binary methods
-% med   25-75   Method
-% -------------------
-% 0.5           @randM2
-% 0.39  0.015   @rbfSvmHogF2 C = 0.00023, gamma = 1
-% 0.30  0.04    @linSvmHogF2 C = 1
-% 0.26  0.02    @nnHog2
-% 0.25  0.02    @pcaNnHog2
-% 0.23  0.05    @linSvmHogCV2 C* = 0.00023
-% 0.18  0.01    @rbfSvmHogCV2 C* = 2, gamma* = 0.00023
-% 0.112 0.11    @nnCnn2
-% 0.104 0.01    @pcaNnCnn2
-% 0.099 0.008   @svmHogCnnBagger2
-% 0.095 0.004   @svmHogCnnKNN2, split = 0.9, k = 5
-% 0.089 0.008   @svmHogCnnMC2  
-% 0.087 0.009   @linSvmPcaCnnCV2 C* = 0.00023, M* = 1300
-% 0.081 0.008   @rbfSvmPcaCnnF2 C* = 3.25, M* = 150, gamma = 0.00023
-% 0.079 0.004   @svmHogCnnCustom2
 methods2 = {
-%     @randM2,
-%     @linSvmHogCV2,
-%     @rbfSvmHogCV2,
-%     @linSvmPcaHogCV2,
-%     @linSvmPcaCnnCV2,
-%     @rbfSvmPcaCnnCV2
-%     @pcaNnHog2,
-%     @pcaNnCnn2,
-%     @nnHog2,
-%     @nnCnn2,
-%     @svmHogCnnMC2
-%     @svmHogCnnKNN2
-%     @svmHogCnnCustom2
-    @svmHogCnnBagger2
+    @randM2
 };
 error2 = zeros(nbRuns, length(methods2));
 
@@ -87,13 +57,8 @@ for r = 1:nbRuns
 end
 
 %% Evaluating multi-class methods
-% med   25-75   Method
-% -------------------
-% 0.75          @randM4
 methods4 = {
-    @randM4,
-%     @pcaNnHog4,
-%     @pcaNnCnn4,
+    @randM4
 };
 error4 = zeros(nbRuns, length(methods4));
 
