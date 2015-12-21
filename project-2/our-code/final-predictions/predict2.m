11cc561a75d549cfe466ac3fdfe246331d687703
Marco Antognini <antognini.marco@gmail.com>
2015-12-21 03:21:18 +0100

    Update prediction codes

--- project-2/our-code/final-predictions/predict2.m
+++ project-2/our-code/final-predictions/predict2.m
@@ -2,14 +2,26 @@
 % Save final predictions for the binary task
 clearvars;
 addpath(genpath('data/'));
-load 'data/finalData.mat';
+load 'data/data.mat';
+load 'data/test.mat';
+
+tr.X.hog = data.hog.train.X;
+tr.X.cnn = data.cnn.train.X;
+tr.cnn.mu = data.cnn.mu;
+tr.hog.mu = data.hog.mu;
+tr.cnn.sigma = data.cnn.sigma;
+tr.hog.sigma = data.hog.sigma;
+tr.y = data.yTrain; % train y are 4-class
+
+te.cnn = normalize(test.X_cnn, tr.cnn.mu, tr.cnn.sigma);
+te.hog = normalize(test.X_hog, tr.hog.mu, tr.hog.sigma);
 
 % define method
 % C* = 3.25, M* = 150, gamma = 0.00023
 method = @(tr, XValid) rbfSvmPcaCnnF2(tr, XValid, 4, 150, 3.25, 0.00023);
 
 % apply method
-Ytest = method(data.tr, data.te);
+Ytest = method(tr, te);
 
 % save yTestPred
 save 'pred_binary.mat' Ytest;
--- project-2/our-code/final-predictions/predict4.m
+++ project-2/our-code/final-predictions/predict4.m
@@ -2,13 +2,25 @@
 % Save final predictions for the binary task
 clearvars;
 addpath(genpath('data/'));
-load 'data/finalData.mat';
+load 'data/data.mat';
+load 'data/test.mat';
+
+tr.X.hog = data.hog.train.X;
+tr.X.cnn = data.cnn.train.X;
+tr.cnn.mu = data.cnn.mu;
+tr.hog.mu = data.hog.mu;
+tr.cnn.sigma = data.cnn.sigma;
+tr.hog.sigma = data.hog.sigma;
+tr.y = data.yTrain; % train y are 4-class
+
+te.cnn = normalize(test.X_cnn, tr.cnn.mu, tr.cnn.sigma);
+te.hog = normalize(test.X_hog, tr.hog.mu, tr.hog.sigma);
 
 % define method
 method = @rbfSvmPcaCnnManualTree4;
 
 % apply method
-Ytest = method(data.tr, data.te);
+Ytest = method(tr, te);
 
 % save yTestPred
 save 'pred_multiclass.mat' Ytest;
