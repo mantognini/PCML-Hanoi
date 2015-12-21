8318f4d7937872f29aa4ec68f9f319bd1c899e2b
pacm <pacm@users.noreply.github.com>
2015-12-21 03:16:07 +0100

    moc-data for final testing

---
+++ project-2/final/mockData.m
@@ -0,0 +1,21 @@
+%
+% Produce mock testing data
+clearvars;
+load 'train/train.mat';
+
+% Add batches
+N = size(train.y, 1);
+test.X_cnn = [];
+test.X_hog = [];
+y = [];
+for i = 1:6
+    idx = randperm(floor(N/ 8));
+
+    test.X_cnn = [test.X_cnn; train.X_cnn(idx, :)];
+    test.X_hog = [test.X_hog; train.X_hog(idx, :)];
+    y = [y train.y(idx, :)];
+end
+
+% Save
+save 'mock-test.mat' test;
+save 'mock-solution.mat' y;
