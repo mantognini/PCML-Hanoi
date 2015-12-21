c0ea32c364a2208cace2c5de159f2fc241e8bf7b
Marco Antognini <antognini.marco@gmail.com>
2015-12-21 03:58:12 +0100

    Update mock -- not so sure if good or bad

--- project-2/final/mockData.m
+++ project-2/final/mockData.m
@@ -8,8 +8,9 @@ N = size(train.y, 1);
 test.X_cnn = [];
 test.X_hog = [];
 y = [];
-for i = 1:6
-    idx = randperm(floor(N/ 8));
+for i = 1:1
+    idx = randperm(N);
+    idx = idx([1:floor(N/6)]);
 
     test.X_cnn = [test.X_cnn; train.X_cnn(idx, :)];
     test.X_hog = [test.X_hog; train.X_hog(idx, :)];
--- project-2/final/testFinalPredictions.m
+++ project-2/final/testFinalPredictions.m
@@ -2,6 +2,39 @@
 % Test
 clearvars;
 addpath(genpath('data/'));
+load 'data/data.mat';
 load 'data/mock-solution.mat';
 load 'data/mock-test.mat';
 
+%%
+
+tr.X.hog = data.hog.train.X;
+tr.X.cnn = data.cnn.train.X;
+tr.cnn.mu = data.cnn.mu;
+tr.hog.mu = data.hog.mu;
+tr.cnn.sigma = data.cnn.sigma;
+tr.hog.sigma = data.hog.sigma;
+tr.y = data.yTrain; % train y are 4-class
+
+te.cnn = normalize(test.X_cnn, tr.cnn.mu, tr.cnn.sigma);
+te.hog = normalize(test.X_hog, tr.hog.mu, tr.hog.sigma);
+
+
+%%
+
+% define method
+% C* = 3.25, M* = 150, gamma = 0.00023
+method = @(tr, XValid) rbfSvmPcaCnnF2(tr, XValid, 4, 150, 3.25, 0.00023);
+
+% apply method
+Ytest = method(tr, te);
+
+BER(toBinary(y, 4), Ytest)
+
+%%
+% define method
+method = @rbfSvmPcaCnnManualTree4;
+
+% apply method
+Ytest = method(tr, te);
+BER(y, Ytest)
